a3899dfa551620963d3441ba8b89e32f9b15cf9f
Phil Fernandez <philvfernandez@gmail.com>
2022-07-03 18:01:39 -0600

    Completed assignment number 2.

--- src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
+++ src/main/java/guru/springframework/msscbrewery/web/controller/CustomerController.java
@@ -2,12 +2,10 @@ package guru.springframework.msscbrewery.web.controller;
 
 import guru.springframework.msscbrewery.web.model.CustomerDto;
 import guru.springframework.msscbrewery.web.services.CustomerService;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.UUID;
 
@@ -24,4 +22,29 @@ public class CustomerController {
     public ResponseEntity<CustomerDto> getCustomerById(@PathVariable("customerId")UUID customerId) {
         return new ResponseEntity<>(customerService.getCustomerById(customerId), HttpStatus.OK);
     }
+
+    @PostMapping // - Create new Customer
+    public ResponseEntity handlePost(@RequestBody CustomerDto customerDto) {
+        CustomerDto savedDto = customerService.saveNewCustomer(customerDto);
+
+        HttpHeaders headers = new HttpHeaders();
+        //TODO: add new hostname to url
+        headers.add("location", "/api/v1/customer" + savedDto.getId().toString());
+
+        return new ResponseEntity(headers, HttpStatus.CREATED);
+
+    }
+
+    @PutMapping({"/{customerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void handleUpdate(@PathVariable("customerId") UUID customerId, @RequestBody CustomerDto customerDto) {
+        customerService.updateCustomer(customerId, customerDto);
+    }
+
+    @DeleteMapping({"/{customerId}"})
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteCustomer(@PathVariable("customerId") UUID customerId) {
+        customerService.deleteById(customerId);
+    }
+
 }
--- src/main/java/guru/springframework/msscbrewery/web/services/CustomerServiceImpl.java
+++ src/main/java/guru/springframework/msscbrewery/web/services/CustomerServiceImpl.java
@@ -1,10 +1,13 @@
 package guru.springframework.msscbrewery.web.services;
 
+import guru.springframework.msscbrewery.web.model.BeerDto;
 import guru.springframework.msscbrewery.web.model.CustomerDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.UUID;
 
+@Slf4j
 @Service
 public class CustomerServiceImpl implements CustomerService{
 
@@ -14,4 +17,26 @@ public class CustomerServiceImpl implements CustomerService{
                 .name("Phil Fernandez")
                 .build();
     }
+
+    @Override
+    public CustomerDto saveNewCustomer(CustomerDto customerDto) {
+        return CustomerDto.builder()
+                .id(UUID.randomUUID())
+                .build();
+    }
+
+    @Override
+    public void updateCustomer(UUID customerId, CustomerDto customerDto) {
+        //TODO: impl -- would add a real impl to update customer
+        log.debug("Updating Customer....");
+    }
+
+    @Override
+    public void deleteById(UUID customerId) {
+        //TODO: impl - would add a real impl to delete customer.
+        log.debug("Deleting Customer....");
+
+    }
+
+
 }
