750d7bd39e6a56138e09f9bffcc6d8ac0a5802fc
Phil Fernandez <philvfernandez@gmail.com>
2022-07-28 16:37:36 -0600

    Added Bean Validation to Beer DTO.

--- src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
+++ src/main/java/guru/springframework/msscbrewery/web/model/BeerDto.java
@@ -5,6 +5,9 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Null;
+import javax.validation.constraints.Positive;
 import java.util.UUID;
 
 @Data //Getters/Setters
@@ -13,8 +16,15 @@ import java.util.UUID;
 @Builder //Builder Pattern implemented
 public class BeerDto {
 
+    @Null
     private UUID id;
+
+    @NotBlank
     private String beerName;
+
+    @NotBlank
     private String beerStyle;
+
+    @Positive
     private Long upc;
 }
